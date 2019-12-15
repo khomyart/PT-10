80ba0b36def148c8648f95afa6a17c1c41a1c325
khomyart <55877483+khomyart@users.noreply.github.com>
2019-12-15 19:53:37 +0200

    contact list made by join

--- imports.php
+++ imports.php
@@ -7,7 +7,7 @@ include './lib/contacts.php';
 /*ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);*/
-
+/*
 initSession();
 $isIndex = basename($_SERVER['SCRIPT_FILENAME']) == 'index.php';
 
