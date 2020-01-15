c3806b3058c49a2b8de3517076044d620dc5b06d
Valentyn Tykhomyrov <khomyart@gmail.com>
2020-01-15 23:00:15 +0200

    Some preperation for classes using
    Actually second time when i need to use classes to work with

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = [
+    'db' => [
+        'server' => '165.22.24.204',
+        'db' => 'p10',
+        'user' => 'valentinvtv',
+        'password' => 'I29y39u68xxx@(#(^*',
+    ]
+];
+
